1f67965d26f4f3340a6399050ea6d7f1fe90abce
SamSbt <samnsabat@gmail.com>
2024-05-10 23:23:41 +0200

    correct notfound page

--- src/screens/NotfoundScreen.js
+++ src/screens/NotfoundScreen.js
@@ -11,7 +11,7 @@ export default class NotfoundScreen extends MainScreen {
     return `
     <style>@import "./src/screens/styles/NotfoundStyle.css"</style>
     <div>${super.render()}</div>
-    <div class="card position-absolute presText" style="width: 20rem">
+    <div class="card position-absolute presText d-flex align-items-center" style="width: 20rem">
       <img src="./src/img/oups.png" class="card-img-oups px-2 mt-2" alt="Sorry, wrong page" draggable="false">
         <div class="card-body">
           <p class="card-text poppins-regular text-center text-black mt-2">
